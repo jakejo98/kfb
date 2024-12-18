fd4fd0091cfae0ae23c479e3889db762a5fa0364
jakejo <jakejo@kakao.com>
2024-12-18 16:04:12 +0900

    Feat : header script 수정

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ import { swiper } from "./lib/swiper.js";
 import { header } from "./common/header.js";
 import { main } from "./pages/main.js";
 
-$(document).ready(function () {
-  loadFile();
-  list();
-  guide();
+$(document).ready(async function () {
+  await loadFile();
+  await list();
+  await guide();
 
   setTimeout(function () {
     swiper();
-    header();
+    header(); 
     main();
   }, 100);
 });
