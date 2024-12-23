622693debeb509c07067d15ea0ce478d25f053a8
wkdqls00 <73662282+wkdqls00@users.noreply.github.com>
2024-12-23 20:18:09 +0900

    Feat : (금리수수료>COFIX>COFIX공시) 헤더 생성, lnb 헤더 생성

--- js/script.js
+++ js/script.js
@@ -6,13 +6,19 @@ import { header } from "./common/header.js";
 import { main } from "./pages/main.js";
 
 $(document).ready(function(){
+  const currentFile = window.location.pathname.split("/").pop();
+
   // 선 실행 함수
   list();
   guide();
   swiper();
+  
   // 로드 후 실행 함수
   loadFile(function(){
     header();
-    main();
+    // main.html 일 경우
+    if (currentFile === "main.html") {
+      main();
+    }
   })
 })
