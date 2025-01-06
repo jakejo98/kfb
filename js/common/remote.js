6914aa4417f06284daebe91dfa3f187bad7b8153
jakejo <jakejo@kakao.com>
2025-01-06 14:34:52 +0900

    Feat : 서브페이지 사이드바 동작 구현

---
+++ js/common/remote.js
@@ -0,0 +1,31 @@
+export function remote(){
+  remoteHandler();
+}
+
+function remoteHandler(){
+  const isRemote = $('#remote-wrap');
+  const firstDepItem = $('.remote-item');
+  const firstDepLink = $('.remote-link');
+  const secondDep = $('.remote-sub');
+  const secondDepItem = $('.remote-sub-item');
+  const secondDepLink = $('.remote-sub-link');
+  const isActive = 'active';
+
+  if(isRemote) {
+    // 리스트 활성화
+    $(firstDepLink).click(function(){
+      if($(this).parent().has(secondDep).length > 0) {
+      $(this).addClass(isActive).parent().siblings().find(firstDepLink).removeClass(isActive);
+      $(this).parent().find(secondDep).addClass(isActive).parent().siblings().find(secondDep).removeClass(isActive);
+      $(this).parent().find(secondDepItem).eq(0).find(secondDepLink).addClass(isActive).parents(firstDepItem).siblings().find(secondDepLink).removeClass(isActive);
+      } else {
+        $(this).addClass(isActive).parent().siblings().find(firstDepLink).removeClass(isActive);
+        $(secondDep).removeClass(isActive);
+      }
+    })
+    // sub-menu-link 활성화
+    $(secondDepLink).click(function(){
+      $(this).addClass(isActive).parent().siblings().find(secondDepLink).removeClass(isActive);
+    })
+  }
+}
