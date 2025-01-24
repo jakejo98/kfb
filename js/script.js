fdd3b6dfcba23c7ce2a66629d1d06677e5ebc004
jakejo <jakejo@kakao.com>
2025-01-24 15:00:12 +0900

    Docs : 메인페이지 반응형 작업중

--- js/script.js
+++ js/script.js
@@ -5,8 +5,29 @@ import { guide } from "./guide/guide.js";
 import { header } from "./common/header.js";
 import { footer } from "./common/footer.js";
 import { remote } from "./common/remote.js";
-import { main } from "./pages/main.js";
+import { commonMain, desktopMain, respondMain } from "./pages/main.js";
 import { admin } from "./common/admin.js";
+
+let windowWidth = 0;
+
+// 윈도우 너비 업데이트 함수
+function updateWidth(){
+  return $(window).width();
+}
+// 윈도우 리사이즈 너비 값 재설정
+$(window).resize(function(){
+  windowWidth = updateWidth();
+  checkMain();
+});
+// 윈도우 너비에 조건부로 작동하는 함수
+function checkMain() {
+  windowWidth = updateWidth();
+  if(windowWidth > 1399) {
+    desktopMain();
+  } else {
+    respondMain();
+  }
+}
 
 $(document).ready(function(){
   const currentFile = window.location.pathname.split("/").pop();
@@ -28,7 +49,8 @@ $(document).ready(function(){
     footer();
     remote();
     if(currentFile.startsWith("main")) {
-      main();
+      commonMain();
+      checkMain();
     }
   })
 })
